86b4c4e3f93421a6203520de4cd7b56c62cc6936
Ritik <ritikvermav5@gmail.com>
2026-01-10 15:52:08 +0530

    initial deploy

--- Backend/redis_server/redis_server.js
+++ Backend/redis_server/redis_server.js
@@ -1,5 +1,7 @@
 import {Redis} from 'ioredis'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const redisServer = new Redis({})
+const redisServer = new Redis(process.env.REDIS_URL)
 
 export default redisServer
